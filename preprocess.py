0509040abcd8b9b9c98425f6a67be31e0d12463d
joe sunshine <joesun02@gmail.com>
2023-11-23 10:31:10 -0500

    loaded csv file removed nan values

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,14 @@
-print('hello world')
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib as plt
+
+
+movies_df = pd.read_csv("movies.csv")
+check_nan = movies_df.isnull().sum()
+print(check_nan)
+movies_df['budget'] = movies_df['budget'].fillna(0)
+movies_df['gross'] = movies_df['gross'].fillna(0)
+movies_df=movies_df.dropna()
+
+print(movies_df.isnull().sum())
